d32d7552a571bafc2bd9541c9067956b47268534
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-11-21 14:28:45 +0530

    modification in file server.js

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
+//server initiation
 const express=require('express');
 const app = express();
+//activating the server on local port 3000
 app.listen(3000, () => {
     console.log("server started at port 3000")
 });
+//routes
+app.get('/',(req,res)=>{
+    res.send("hello how are u")
+})
+app.post('/api/cars',(req,res)=>{
+    const{name,brand}=req.body;
+    console.log(name);
+    console.log(brand);
+    res.send("car submitted successfully");
+})
